6c44aa73b62ecd715afa91c95c45c8872b5c670f
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2016-06-21 14:05:51 +0300

    Changed reader dor sleeping after every 1000 messages

--- src/main/java/diploma/queue/Reading.java
+++ src/main/java/diploma/queue/Reading.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Nikita on 19.06.2016.
@@ -19,11 +20,18 @@ public class Reading implements Runnable {
 
     public void run() {
         try (BufferedReader br = new BufferedReader(new FileReader(path.toString()))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                queue.add(line);
-                Thread.sleep(1);
-            }
+            String line = null;
+            do {
+                long start = System.currentTimeMillis();
+                for (int i = 0; i < 1000; i++) {
+                    line = br.readLine();
+                    if (line != null)
+                        queue.add(line);
+                    else break;
+                }
+                long finish = System.currentTimeMillis() - start;
+                Thread.sleep(1000 - finish);
+            } while (line != null);
         }
         catch (InterruptedException | IOException ex) {
             ex.printStackTrace();
